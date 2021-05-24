8469206a3affd985a9cd0b898ee98e059d78cc70
akbar putera w <akbar@five-code.com>
2021-05-24 11:26:30 +0700

    a

--- app/src/main/java/com/akbar/longwalks/CardViewAdapter.kt
+++ app/src/main/java/com/akbar/longwalks/CardViewAdapter.kt
@@ -1,40 +1,35 @@
 package com.akbar.longwalks
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import java.util.*
 
-class dataCardView{
-    var name: String = ""
-    var title: String = ""
-    var image: String = ""
-}
+data class DataCardView (
+    val name: String,
+    val title: String
+)
 
-class CardViewAdapter(private var data: List<dataCardView>, private var listener: (dataCardView) -> Unit): RecyclerView.Adapter<CardViewAdapter.ViewHolder>() {
+class CardViewAdapter(private var data: List<DataCardView>): RecyclerView.Adapter<CardViewAdapter.CardHolder>() {
 
-    lateinit var contextAdapter: Context
-
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardHolder {
+        return CardHolder(LayoutInflater.from(parent.context).inflate(R.layout.for_you_item_row, parent, false))
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        contextAdapter = parent.context
-        var inflateView = layoutInflater.inflate(R.layout.for_you_item_row, parent, false)
-        return ViewHolder(inflateView)
+    override fun onBindViewHolder(holder: CardHolder, position: Int) {
+        holder.bindHero(data[position])
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun getItemCount(): Int = data.size
 
-    }
+    class CardHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val title:TextView = view.findViewById(R.id.textView)
+        private val desc:TextView = view.findViewById(R.id.textView7)
 
-    override fun getItemCount(): Int {
-        return 1
+        fun bindHero(data: DataCardView) {
+            title.text = data.title
+            desc.text = data.name
+        }
     }
 }
--- app/src/main/java/com/akbar/longwalks/MainActivity.kt
+++ app/src/main/java/com/akbar/longwalks/MainActivity.kt
@@ -2,18 +2,32 @@ package com.akbar.longwalks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var rv_card: RecyclerView
+    lateinit var rvCard: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val listData = listOf(
+            DataCardView(title = "Telenovela", name = "Tentang Cinta"),
+            DataCardView(title = "Telenovela", name = "Tentang Cinta"),
+            DataCardView(title = "Telenovela", name = "Tentang Cinta"),
+            DataCardView(title = "Telenovela", name = "Tentang Cinta")
+        )
+        val cardAdapter = CardViewAdapter(listData)
 
-        rv_card.adapter = CardViewAdapters()
+        rvCard = findViewById(R.id.recyclerView)
+
+        rvCard.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
+            adapter = cardAdapter
+        }
+
     }
 }
